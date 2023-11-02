f66137b6863779d342cf33ba01a9aabbaa08495b
Rafael Estrada <ntaticat@gmail.com>
2023-11-01 20:42:01 -0600

    modify custom pipes

--- src/app/pipes/activities-filter.pipe.ts
+++ src/app/pipes/activities-filter.pipe.ts
@@ -2,56 +2,94 @@ import { Pipe, PipeTransform } from '@angular/core';
 import { IActivityApplication } from '../data/activities.interfaces';
 
 @Pipe({
-  name: 'activitiesFilter'
+  name: 'activitiesFilter',
 })
 export class ActivitiesFilterPipe implements PipeTransform {
-
-  transform(activities: IActivityApplication[], textoBusqueda: string, filtroBusqueda: string): IActivityApplication[] {
-
-    if(filtroBusqueda == "actividad") {
+  transform(
+    activities: IActivityApplication[],
+    textoBusqueda: string,
+    filtroBusqueda: string
+  ): IActivityApplication[] {
+    if (filtroBusqueda == 'actividad') {
       return activities.filter((activity) => {
+        const nombreActividad = activity.nombre_actividad
+          .normalize('NFD')
+          .replace(/[\u0300-\u036f]/g, '')
+          .toLocaleLowerCase();
+        const nombrePrograma = activity.nombre_programa
+          .normalize('NFD')
+          .replace(/[\u0300-\u036f]/g, '')
+          .toLocaleLowerCase();
+        const textoBuscar = textoBusqueda
+          .normalize('NFD')
+          .replace(/[\u0300-\u036f]/g, '')
+          .toLocaleLowerCase();
 
-        const nombreActividad = activity.nombre_actividad.normalize("NFD").replace(/[\u0300-\u036f]/g, "").toLocaleLowerCase();
-        const nombrePrograma = activity.nombre_programa.normalize("NFD").replace(/[\u0300-\u036f]/g, "").toLocaleLowerCase();
-        const textoBuscar = textoBusqueda.normalize("NFD").replace(/[\u0300-\u036f]/g, "").toLocaleLowerCase();
-
-        return nombreActividad.includes(textoBuscar) || nombrePrograma.includes(textoBuscar);
+        return (
+          nombreActividad.includes(textoBuscar) ||
+          nombrePrograma.includes(textoBuscar)
+        );
       });
     }
 
-    if(filtroBusqueda == "direccion") {
+    if (filtroBusqueda == 'direccion') {
       return activities.filter((activity) => {
-
-        const domicilioPrograma = activity.domicilio_programa.normalize("NFD").replace(/[\u0300-\u036f]/g, "").toLocaleLowerCase();
-        const textoBuscar = textoBusqueda.normalize("NFD").replace(/[\u0300-\u036f]/g, "").toLocaleLowerCase();
+        const domicilioPrograma = activity.domicilio_programa
+          .normalize('NFD')
+          .replace(/[\u0300-\u036f]/g, '')
+          .toLocaleLowerCase();
+        const textoBuscar = textoBusqueda
+          .normalize('NFD')
+          .replace(/[\u0300-\u036f]/g, '')
+          .toLocaleLowerCase();
 
         return domicilioPrograma.includes(textoBuscar);
       });
     }
 
-    if(filtroBusqueda == "prestatario") {
+    if (filtroBusqueda == 'prestatario') {
       return activities.filter((activity) => {
+        const nombrePrestatario = activity.nombre_prestatario
+          .normalize('NFD')
+          .replace(/[\u0300-\u036f]/g, '')
+          .toLocaleLowerCase();
+        const nombrePrestatarioLargo = activity.nombre_largo_prestatario
+          .normalize('NFD')
+          .replace(/[\u0300-\u036f]/g, '')
+          .toLocaleLowerCase();
+        const textoBuscar = textoBusqueda
+          .normalize('NFD')
+          .replace(/[\u0300-\u036f]/g, '')
+          .toLocaleLowerCase();
 
-        const nombrePrestatario = activity.nombre_prestatario.normalize("NFD").replace(/[\u0300-\u036f]/g, "").toLocaleLowerCase();
-        const nombrePrestatarioLargo = activity.nombre_largo_prestatario.normalize("NFD").replace(/[\u0300-\u036f]/g, "").toLocaleLowerCase();
-        const textoBuscar = textoBusqueda.normalize("NFD").replace(/[\u0300-\u036f]/g, "").toLocaleLowerCase();
-
-        return nombrePrestatario.includes(textoBuscar) || nombrePrestatarioLargo.includes(textoBuscar);
+        return (
+          nombrePrestatario.includes(textoBuscar) ||
+          nombrePrestatarioLargo.includes(textoBuscar)
+        );
       });
     }
 
-    if(filtroBusqueda == "objetivo") {
+    if (filtroBusqueda == 'objetivo') {
       return activities.filter((activity) => {
+        const objetivo = activity.objetivo_programa
+          .normalize('NFD')
+          .replace(/[\u0300-\u036f]/g, '')
+          .toLocaleLowerCase();
+        const justificacion = activity.justificacion_programa
+          .normalize('NFD')
+          .replace(/[\u0300-\u036f]/g, '')
+          .toLocaleLowerCase();
+        const textoBuscar = textoBusqueda
+          .normalize('NFD')
+          .replace(/[\u0300-\u036f]/g, '')
+          .toLocaleLowerCase();
 
-        const objetivo = activity.objetivo_programa.normalize("NFD").replace(/[\u0300-\u036f]/g, "").toLocaleLowerCase();
-        const justificacion = activity.justificacion_programa.normalize("NFD").replace(/[\u0300-\u036f]/g, "").toLocaleLowerCase();
-        const textoBuscar = textoBusqueda.normalize("NFD").replace(/[\u0300-\u036f]/g, "").toLocaleLowerCase();
-
-        return objetivo.includes(textoBuscar) || justificacion.includes(textoBuscar);
+        return (
+          objetivo.includes(textoBuscar) || justificacion.includes(textoBuscar)
+        );
       });
     }
 
     return activities;
   }
-
 }
--- src/app/pipes/activities-type.pipe.ts
+++ src/app/pipes/activities-type.pipe.ts
@@ -2,30 +2,32 @@ import { Pipe, PipeTransform } from '@angular/core';
 import { IActivityApplication } from '../data/activities.interfaces';
 
 @Pipe({
-  name: 'activitiesType'
+  name: 'activitiesType',
 })
 export class ActivitiesTypePipe implements PipeTransform {
-
-  transform(activities: IActivityApplication[], activityType: string | undefined): IActivityApplication[] {
-
-    if(activityType === "liked") {
-      console.log("ES LIKE!!!")
-      const likedArr = activities.filter((activity) => activity.actividad_liked);
+  transform(
+    activities: IActivityApplication[],
+    activityType: string | undefined
+  ): IActivityApplication[] {
+    if (activityType === 'liked') {
+      const likedArr = activities.filter(
+        (activity) => activity.actividad_liked
+      );
       console.log(likedArr);
       activities.splice(0, activities.length, ...likedArr);
 
       return activities;
     }
 
-    if(activityType === "removed") {
-      const removedArr = activities.filter((activity) => activity.actividad_removed);
+    if (activityType === 'removed') {
+      const removedArr = activities.filter(
+        (activity) => activity.actividad_removed
+      );
       console.log(removedArr);
       activities.splice(0, activities.length, ...removedArr);
 
       return activities;
-    }
-
-    else {
+    } else {
       return activities;
     }
   }
